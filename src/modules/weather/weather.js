82a25bbb072e7772aa7d68a53521f948f87b2973
Sarvarbek Berdiyev <berdiyevs2006@gmail.com>
2026-03-10 00:58:22 +0500

    integrate OpenWeather API and display real-time weather data

--- src/modules/weather/weather.js
+++ src/modules/weather/weather.js
@@ -1,29 +1,18 @@
-const changeLocation = document.getElementById('change-location')
-const KEY = `8f74f27f57f990f203e46d42d77bc30e`;
+const apiKey = '8f74f27f57f990f203e46d42d77bc30e';
+const form = document.getElementById('change-location');
+const cityInput = document.getElementById('form-control')
 
-const getData = async (city) => {
-    const base = `https://api.openweathermap.org/data/2.5/weather`;
-    const query = `?q=${city}&units=metric&appid=${KEY}`;
-
-    const req = await fetch(base + query);
-    const data = await req.json();
-
-    return data
+async function getWeather() {
+    const cityValue = cityInput.value.trim()
+    const getData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric&lang=uz`)
+    const data = await getData.json()
+    const getTemp = Math.round(data.main.temp)
+    document.querySelector('.return').innerHTML = `
+        <h1 class='element'>${cityValue}da harorat ${getTemp}°C kuningiz hayrli o'tsin</h1>
+    `
 }
 
-const getWeather = async (city) => {
-    const data = await getData(city)
-    console.log(data)
-}
-
-changeLocation.addEventListener("submit", (e) => {
-    e.preventDefault()
-    const cityName = changeLocation.city.value.trim()
-    changeLocation.reset()
-    getWeather(cityName)
-})
-
-
-
-
-
+form.addEventListener('submit', (e) => {
+    e.preventDefault(); // MUHIM: Sahifa yangilanib ketishini to'xtatadi
+    getWeather();       // Mana endi funksiyangiz ishga tushadi!
+});
